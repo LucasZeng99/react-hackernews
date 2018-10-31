fceb81a7d44248040a095c71009119e2eac52a85
Lucas Zeng <lucaszeng99@gmail.com>
2018-10-31 10:32:41 -0600

    add url host names, add display items

--- src/components/ListCard.js
+++ src/components/ListCard.js
@@ -22,22 +22,40 @@ export function titleLink (item) {
     return <Link to={'item/' + item.id}>{title}</Link>
   }
 }
+
+export function urlHost (url) {
+  let secondSlash, thirdSlash = 0;
+  for (let i = 0; i < 3; i++) {
+    thirdSlash = url.indexOf('/', thirdSlash + 1)
+    if (i < 2) {
+      secondSlash = thirdSlash
+    }
+  }
+  return url.slice(secondSlash + 1, thirdSlash)
+}
+
 export class ListCard extends Component{
   timeDiff = (time) => timeDiff(time) // map modular functions to local methods
   titleLink = (item) => titleLink(item)
   render () {
-    let {by, id, score, time, type, kids} = this.props.item
+    console.log(this.props.item)
+    let {by, id, score, time, type, descendants} = this.props.item
     let [timeDiff, phrase] = this.timeDiff(time)
     
     return <div className="list-item">
       <div className="li-score"> {score} </div>
-      <div className="li-title-link"> {this.titleLink(this.props.item)} </div>
-      <div className="li-by">By {by} </div>
-      <div className="li-time"> {timeDiff} {phrase} ago </div>
-      <div className="li-item-link">
+      
+      <div className="li-title">
+        <div className="li-title-link"> {this.titleLink(this.props.item)} </div>
+        {urlHost(this.props.item.url)}
+      </div>
+
+      <div className="li-meta">
+        <div className="li-by">by {by} </div>
+        <div className="li-time"> {timeDiff} {phrase} ago </div>
         <Link to={'item/' + id}>
-          {kids && type !== 'job' ? (
-            <p>{kids.length} comments</p>
+          {descendants && type !== 'job' ? (
+            <p>{descendants} comments</p>
           ) : (
             <p>{type}</p>
           )
